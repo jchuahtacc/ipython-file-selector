acdf974bac97cdfaad7e547a704c17f89b0c84d0
jchuahtacc <jchuah@tacc.utexas.edu>
2016-10-13 11:20:33 -0500

    Renamed class

--- ipython_file_selector/ipython_file_selector.py
+++ ipython_file_selector/ipython_file_selector.py
@@ -1,7 +1,7 @@
 from IPython.html.widgets import DOMWidget
 from IPython.utils.traitlets import Unicode, Int
 
-class FileSelector(DOMWidget):
+class IPFileSelector(DOMWidget):
     _view_module = Unicode('nbextensions/ipython_file_selector/ipython_file_selector', sync=True)
     _view_name = Unicode('IPFileSelector', sync=True)
     count = Int(sync=True)
