9ff2cdd87427c26e90a07c1acb0e61126c281c88
jchuahtacc <jchuah@tacc.utexas.edu>
2016-10-17 09:25:52 -0500

    Message passing between front and back ends

--- ipython_file_selector/ipython_file_selector.js
+++ ipython_file_selector/ipython_file_selector.js
@@ -11,17 +11,11 @@ define(['jquery', path ], function($, widget) {
     var IPFileSelector = widget.DOMWidgetView.extend({
         render: function() {
             IPFileSelector.__super__.render.apply(this, arguments);
-            this._count_changed();
-            this.listenTo(this.model, 'change:count', this._count_changed, this);
             $(this.el).text("Hello world");
-            console.log("IP File Selector Hello World");
+            var msg = { 'type' : 'init' };
+            this.send(msg);
         },
 
-        _count_changed: function() {
-            var old_value = this.model.previous('count');
-            var new_value = this.model.get('count');
-            $(this.el).text(String(old_value) + ' -> ' + String(new_value));
-        }
     });
 
     return {
--- ipython_file_selector/ipython_file_selector.py
+++ ipython_file_selector/ipython_file_selector.py
@@ -1,7 +1,14 @@
 from ipywidgets import DOMWidget
-from traitlets import Unicode, Int
+from traitlets import Unicode
 
 class IPFileSelector(DOMWidget):
     _view_module = Unicode('nbextensions/ipython_file_selector/ipython_file_selector', sync=True)
     _view_name = Unicode('IPFileSelector', sync=True)
-    count = Int(sync=True)
+
+    def __init__(self, *args, **kwargs):
+        super(IPFileSelector, self).__init__(*args, **kwargs)
+        self.on_msg(self.handleMsg)
+
+    def handleMsg(self, widget, content, buffers=None):
+        print(content['type'])
+
